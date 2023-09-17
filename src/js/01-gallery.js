f9cbed4ca85adb0c5726ef37ae0e53a37835ffef
Toy Santana António <toysan3@hotmail.com>
2023-09-18 00:39:58 +0200

    Update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,19 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>
+  `;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
